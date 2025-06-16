683db693a2639a4fe42bbf983796603a8ae815c9
ErfanGerami <e.geramizadeh13821359@gmail.com>
2025-06-16 15:02:48 +0330

    writing ssis from scratch again

---
+++ DW/fill_dim_date.sql
@@ -0,0 +1,18 @@
+EXEC sp_configure 'show advanced options', 1;
+RECONFIGURE;
+EXEC sp_configure 'Ad Hoc Distributed Queries', 1;
+RECONFIGURE;
+
+BULK INSERT shared.dim_date
+FROM 'C:\Users\CD CENTER\Desktop\DBProject\dim_date.csv'
+WITH (
+    FORMAT = 'CSV',
+    FIRSTROW = 2,  
+    FIELDTERMINATOR = ',',
+    ROWTERMINATOR = '\n',
+    TABLOCK,
+    CODEPAGE = '65001'  
+);
+
+
+select * from shared.dim_date where date_id='2025-06-15'
